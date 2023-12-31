d6bf903bc269bae94deca0911d76f68113a12b14
DevilSlayer1 <153759200+DevilSlayer1@users.noreply.github.com>
2024-01-01 01:14:03 +0530

    Update covidNewsApi.js

--- covidNewsApi.js
+++ covidNewsApi.js
@@ -1,9 +1,8 @@
-/*
 document.addEventListener('DOMContentLoaded', () => {
     //const apiKey = "a43095fc9fbb40e791dc3b251a4d3449";
     const apiUrl = `news.json`;
     
-    async function getNewsArticle() {
+    async function getNewsArticle(a,b) {
         try {
             const addnews = document.querySelector('.news-section');
             let response = await fetch(apiUrl);
@@ -12,8 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 let data = await response.json();
     
                 if (data.status === "ok" && data.articles.length > 0) {
-                    console.log(data.articles.length);
-                    for (let i = 0; i < data.articles.length; i++) {
+                    //console.log(data.articles.length);
+
+                    //const data = JSON.parse(jsonData);
+
+                    // Sort the articles by the "publishedAt" date in ascending order
+                    data.articles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
+
+                    // Convert the sorted data back to JSON
+                    const sortedJsonData = JSON.stringify(data, null, 2);
+
+                    //console.log(sortedJsonData);
+                    for (let i = a; i < b; i++) {
                         if(data.articles[i].urlToImage!=null)
                         {
                             let newnews = `<article class="news-card" >
@@ -27,9 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         </article>`;
                         addnews.insertAdjacentHTML('beforeend', newnews);
                         }
-                        
                     }
-                   
                 } else {
                     console.error("News API request failed or no articles found.");
                 }
@@ -41,68 +49,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    getNewsArticle();
-});
-*/
-function shuffleArray(array) {
-    const shuffled = array.slice();
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-}
-document.addEventListener('DOMContentLoaded', () => {
-    //const apiKey = "a43095fc9fbb40e791dc3b251a4d3449";
-    const apiUrl = `news.json`;
-    
-    async function getNewsArticle() {
-        try {
-            const addnews = document.querySelector('.news-section');
-            let response = await fetch(apiUrl);
-    
-            if (response.status === 200) {
-                let data = await response.json();
-    
-                if (data.status === "ok" && data.articles.length > 0) {
-                    console.log(data.articles.length);
-                    let length=data.articles.length;
-                    const shuffledArticles = shuffleArray(data.articles);
-                    addnews.innerHTML = '';
-                    for (let i = 0; i <12; i++) {
-                        
-                            const article = shuffledArticles[i];
-                            if(article.urlToImage!=null)
-                            {
-                                const newnews = `<article class="news-card">
-                            <img src="${article.urlToImage}" alt="Health News Image">
-                            <div class="news-content">
-                                <h2>${article.title}</h2>
-                                <p class="article-meta">Published on <span>${article.publishedAt}</span> | By: <span>${article.author}</span></p>
-                                <p class="article-summary">${article.description}</p>
-                                <a href="${article.url}" class="read-more-link">Read More</a>
-                            </div>
-                        </article>`;
-                            addnews.insertAdjacentHTML('beforeend', newnews);
-                        }
-                            
-                        }
-                        
-                   
-                } else {
-                    console.error("News API request failed or no articles found.");
-                }
-            } else {
-                console.error(`Error: ${response.status} - ${response.statusText}`);
-            }
-        } catch (error) {
-            console.error("Error fetching news data:", error);
-        }
-    }
-    
-    getNewsArticle();
+    getNewsArticle(0,13);
     const reload=document.getElementsByClassName('btn')[2];
+    let c=13;
     reload.addEventListener('click',()=>{
-        getNewsArticle();
-    })
+        getNewsArticle(c,c+13);
+        c=c+13;
+        console.log(c);
+    });
 });
